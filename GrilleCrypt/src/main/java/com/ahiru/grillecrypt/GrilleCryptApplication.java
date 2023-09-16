328f945b56576e55f58751fc3f90154fb15435d8
ahiru <ahiru@ahiru-pc>
2023-09-16 19:35:19 +0400

    First commit

---
+++ GrilleCrypt/src/main/java/com/ahiru/grillecrypt/GrilleCryptApplication.java
@@ -0,0 +1,13 @@
+package com.ahiru.grillecrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrilleCryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrilleCryptApplication.class, args);
+	}
+
+}
---
+++ GrilleCrypt/src/test/java/com/ahiru/grillecrypt/GrilleCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahiru.grillecrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrilleCryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
