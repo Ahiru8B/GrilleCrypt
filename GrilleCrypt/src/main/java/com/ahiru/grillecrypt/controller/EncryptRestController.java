1c76d3ef11680a4ddd72da32bb83258460325fc4
ahiru <ahiru@ahiru-pc>
2023-09-22 14:13:29 +0400

    Матрица меняет размерность

--- GrilleCrypt/src/main/java/com/ahiru/grillecrypt/controller/EncryptRestController.java
+++ GrilleCrypt/src/main/java/com/ahiru/grillecrypt/controller/EncryptRestController.java
@@ -1,5 +1,7 @@
 package com.ahiru.grillecrypt.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ahiru.grillecrypt.model.Index;
 import com.ahiru.grillecrypt.service.GrilleCrypt;
 
 @RestController
@@ -22,5 +25,11 @@ public class EncryptRestController {
 	public void setSize(@PathVariable Integer size) {
 		log.info("setSize size = {}", size);
 		this.grilleCrypt.setSize(size);
+	}
+	
+	@GetMapping("/getFreeIndex")
+	public List<Index> getFreeIndex() {
+		log.info("free index = {}", this.grilleCrypt.getFreeIndexes());
+		return this.grilleCrypt.getFreeIndexes();
 	}
 }
--- GrilleCrypt/src/main/resources/static/scrypt.js
+++ GrilleCrypt/src/main/resources/static/scrypt.js
@@ -1,10 +1,30 @@
 $(document).ready(() => {
 	$('select').on('change', function(e) {
+		let size = this.value;
 		$.ajax({
-			url: "./api/setSize/" + this.value,
+			url: "./api/setSize/" + size,
 			type: 'GET',
 			success: function(res) {
-				console.log("OK")
+				let matrix = "";
+				for(let i = 0; i < size; i++) {
+					matrix += "<div>";
+					for(let j = 0; j < size; j++) {
+						matrix += '<button id="' + i + '-' + j + '" type="button" class="btn btn-secondary matrix-button" disabled></button>'
+					}
+					matrix += "</div>";
+				}
+				
+				$("#matrix").html(matrix);
+				$.ajax({
+					url: "./api/getFreeIndex",
+					type: 'GET',
+					success: function(free) {
+						for(let i = 0; i < free.length; i++) {
+							id = free[i]["row"] + "-" + free[i]["column"];
+							$("#" + id).prop('disabled', false);
+						}
+					}
+				});
 			}
 		});
 	});
